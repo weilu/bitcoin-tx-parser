4dc6ef0d15eb0c55e2a00fbb40a70e04fb048f9e
Wei Lu <luwei.here@gmail.com>
2014-08-25 14:25:09 +0800

    reorganize functions

--- index.js
+++ index.js
@@ -3,6 +3,59 @@ var Address = require('bitcoinjs-lib').Address
 var networks = require('bitcoinjs-lib').networks
 var assert = require('assert')
 
+function parse(txGraph, addresses, network) {
+  if(addresses == null || addresses.length < 1) return txGraph;
+
+  network = network || networks.testnet //FIXME
+  var results = []
+
+  var tails = txGraph.getTails()
+  assertEmptyNodes(tails)
+
+  var tailsNext = tails.reduce(function(memo, node) {
+    return memo.concat(node.nextNodes)
+  }, [])
+  assertNoneFundingNodes(tailsNext, addresses, network)
+
+  txGraph.heads.forEach(function(node) {
+    calculateFeesAndValuesForPath(node, addresses, network)
+  })
+
+  return txGraph
+}
+
+function assertEmptyNodes(nodes) {
+  assert(nodes.every(function(node) {
+    return node.tx == null
+  }), "expect graph tails to contain only tx ids")
+}
+
+function assertNoneFundingNodes(nodes, addresses, network) {
+  assert(nodes.every(function(node) {
+      var outputAddresses = node.tx.outs.map(function(output) {
+        return Address.fromOutputScript(output.script, network).toString()
+      })
+      var partOfOutput = outputAddresses.some(function(address) {
+        addresses.indexOf(address) >= 0
+      })
+
+      return !partOfOutput
+  }), "expect graph to contain the input transactions of the first funding transactions")
+
+}
+
+function calculateFeesAndValuesForPath(node, addresses, network) {
+  if(node.prevNodes.length === 0) return;
+
+  var feeAndValue = calculateFeeAndValue(node, addresses, network)
+  node.tx.fee = feeAndValue.fee
+  node.tx.value = feeAndValue.value
+
+  node.prevNodes.forEach(function(n) {
+    calculateFeesAndValuesForPath(n, addresses, network)
+  })
+}
+
 function calculateFeeAndValue(node, addresses, network) {
   var tx = node.tx
 
@@ -47,59 +100,6 @@ function calculateFeeAndValue(node, addresses, network) {
     fee: inputFeeAndValue.fee - outputFeeAndValue.fee,
     value: outputFeeAndValue.value - inputFeeAndValue.value
   }
-}
-
-function calculateFeesAndValuesForPath(node, addresses, network) {
-  if(node.prevNodes.length === 0) return;
-
-  var feeAndValue = calculateFeeAndValue(node, addresses, network)
-  node.tx.fee = feeAndValue.fee
-  node.tx.value = feeAndValue.value
-
-  node.prevNodes.forEach(function(n) {
-    calculateFeesAndValuesForPath(n, addresses, network)
-  })
-}
-
-function parse(txGraph, addresses, network) {
-  if(addresses == null || addresses.length < 1) return txGraph;
-
-  network = network || networks.testnet //FIXME
-  var results = []
-
-  var tails = txGraph.getTails()
-  assertEmptyNodes(tails)
-
-  var tailsNext = tails.reduce(function(memo, node) {
-    return memo.concat(node.nextNodes)
-  }, [])
-  assertNoneFundingNodes(tailsNext, addresses, network)
-
-  txGraph.heads.forEach(function(node) {
-    calculateFeesAndValuesForPath(node, addresses, network)
-  })
-
-  return txGraph
-}
-
-function assertEmptyNodes(nodes) {
-  assert(nodes.every(function(node) {
-    return node.tx == null
-  }), "expect graph tails to contain only tx ids")
-}
-
-function assertNoneFundingNodes(nodes, addresses, network) {
-  assert(nodes.every(function(node) {
-      var outputAddresses = node.tx.outs.map(function(output) {
-        return Address.fromOutputScript(output.script, network).toString()
-      })
-      var partOfOutput = outputAddresses.some(function(address) {
-        addresses.indexOf(address) >= 0
-      })
-
-      return !partOfOutput
-  }), "expect graph to contain the input transactions of the first funding transactions")
-
 }
 
 module.exports = parse
